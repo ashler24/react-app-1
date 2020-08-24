0634452e1505af249b3cb05d739b4f0d13a59abd
Abhijeet <pawar2467@gmail.com>
2020-08-24 17:45:53 +0530

    single page apllication

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Link } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import Main from './components/MainComponent'
 import './App.css';
 
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,8 +3,11 @@ import Header from './HeaderComponent'
 import Footer from './FooterComponent'
 import Home from './HomeComponent'
 import Menu from './MenuComponent'
-import Dishdetail from './DishdetailComponent'
+import Contact from './ContactComponent'
 import { DISHES } from '../shared/dishes'
+import { COMMENTS } from '../shared/comments'
+import { LEADERS } from '../shared/leaders'
+import { PROMOTIONS } from '../shared/promotions'
 import { Switch, Route, Redirect } from 'react-router-dom'
 
 
@@ -14,6 +17,9 @@ class Main extends Component {
 
         this.state = {
             dishes: DISHES,
+            comments: COMMENTS,
+            promotions: PROMOTIONS,
+            leaders: LEADERS
 
         }
     }
@@ -22,7 +28,10 @@ class Main extends Component {
     render() {
         const HomePage = () => {
             return (
-                <Home />
+                <Home dish={this.state.dishes.filter(dish => dish.featured)[0]}
+                    promotion={this.state.promotions.filter(promo => promo.featured)[0]}
+                    leader={this.state.leaders.filter(leader => leader.featured)[0]}
+                />
             )
         }
         return (
@@ -31,6 +40,7 @@ class Main extends Component {
                 <Switch>
                     <Route path="/home" component={HomePage} />
                     <Route exact path="/menu" component={() => <Menu dishes={this.props.dishes} />} />
+                    <Route path="/contactus" component={Contact} />
                     <Redirect to="/home" />
                 </Switch>
                 <Footer />
